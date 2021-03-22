fc41948febf59619cc972a58a0a378212d0b7a53
Bruno Østergaard Domazet <bruno.domazet@protonmail.com>
2021-03-22 22:19:03 +0100

    working client, writes to file

--- client.go
+++ client.go
@@ -2,23 +2,30 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/uniplaces/carbon"
 )
 
 type Reminder struct {
-	Start   float64 `json:"start"`
-	End     float64 `json:"end"`
 	Message string  `json:"message,omitempty"`
+	Start   float64 `json:"start"`
+	// End     float64 `json:"end"`
 	// Repeat  Interval `json:"repeat,omitempty"`
 }
+
+var modifierUnits = `m|h|d`
+var benchMark = time.Now()
 
 // type Interval struct {
 // 	// minutes,hours,days,weeks,months,years
@@ -26,13 +33,7 @@ type Reminder struct {
 // 	Interval float64 `json:"interval,omitempty"`
 // }
 
-func greet(name string) string {
-	m := fmt.Sprintln("from greet is", name)
-	return m
-}
-
 func readDb(filepath string, data *[]Reminder) (bool, error) {
-	start := time.Now()
 
 	file, err := ioutil.ReadFile(filepath)
 	if err != nil {
@@ -48,12 +49,11 @@ func readDb(filepath string, data *[]Reminder) (bool, error) {
 		return false, err
 	}
 
-	log.Debug().Caller().Float64("took", time.Since(start).Seconds()).Send()
+	log.Debug().Caller().Float64("took", time.Since(benchMark).Seconds()).Msg("readDb")
 	return true, nil
 }
 
 func writeDb(filepath string, data []Reminder) (bool, error) {
-	start := time.Now()
 
 	jsonString, err := json.Marshal(data)
 	if err != nil {
@@ -65,7 +65,7 @@ func writeDb(filepath string, data []Reminder) (bool, error) {
 		return false, err
 	}
 
-	log.Debug().Caller().Float64("took", time.Since(start).Seconds()).Send()
+	log.Debug().Caller().Float64("took", time.Since(benchMark).Seconds()).Msg("writeDb")
 	return true, nil
 }
 
@@ -79,38 +79,84 @@ func isFlagSet(name string) bool {
 	return found
 }
 
-func parseTimeString(timeString string) float64 {
-	// now := carbon.Now()
+func parseTimeString(timeString string, initialDateTime *carbon.Carbon) (*carbon.Carbon, error) {
+	now := initialDateTime
 
 	// modifier present
 	if strings.HasPrefix(timeString, "+") {
-		// TODO regex it
-		return 2
+		r := regexp.MustCompile(`\+(?P<mod>\d)(?P<unit>(` + modifierUnits + `)+)`)
+		modifiers, err := regMap(timeString, r)
+		if err != nil {
+			return nil, err
+		}
+
+		intUnit, err := strconv.Atoi(modifiers["mod"])
+		if err != nil {
+			return nil, err
+		}
+
+		switch modifiers["unit"] {
+		case "m":
+			now = now.AddMinutes(intUnit)
+			break
+		case "h":
+			now = now.AddHours(intUnit)
+			break
+		case "d":
+			now = now.AddDays(intUnit)
+			break
+		}
 	}
 
 	// timestamp present
 	if strings.Contains(timeString, "@") {
-		// hourMinute := strings.Split(strings.Split(timeString, "@")[1], ":")
-		// t1, _ = carbon.Create(2012, 1, 31, 12, 0, 0, 0, "Europe/Rome")
+		r := regexp.MustCompile(`\@(?P<h>\d{2})\:?(?P<m>\d{2})?`)
+		hourMinute, err := regMap(timeString, r)
+		if err != nil {
+			log.Debug().Caller().Float64("took", time.Since(benchMark).Seconds()).Msg(err.Error())
+			return nil, err
+		}
+		err = now.SetTimeFromTimeString(hourMinute["h"] + ":" + hourMinute["m"] + ":05")
+		if err != nil {
+			log.Debug().Caller().Float64("took", time.Since(benchMark).Seconds()).Msg(err.Error())
+			return nil, err
+		}
 
 	}
 
-	return 1
+	return now, nil
+}
+
+func regMap(s string, r *regexp.Regexp) (map[string]string, error) {
+	vals := r.FindStringSubmatch(s)
+	keys := r.SubexpNames()
+	d := make(map[string]string)
+
+	// not the same amount of keys vs values
+	if len(vals) != len(keys) {
+		return nil, errors.New("Some arguments are missing or contain invalid values!")
+	}
+
+	// map it
+	for i := 1; i < len(keys); i++ {
+		d[keys[i]] = vals[i]
+	}
+	return d, nil
 }
 
 func main() {
-	benchMark := time.Now()
+
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
 	// Flags
-	debug := flag.Bool("v", false, "Verbose, sets log level to debug")
+	debug := flag.Bool("v", false, "Verbose, sets log level to 'debug'")
 	help := flag.Bool("h", false, "This help message")
 
-	msgFlag := flag.String("msg", "", "(required) The what")
-	startFlag := flag.String("start", "", "(required) The when. Format: '+N[min|hour|day][@hh:mm]'")
-	endFlag := flag.String("end", "", "(optional) The end. Same as 'start'")
+	msgFlag := flag.String("msg", "", "The what")
+	startFlag := flag.String("start", "", "The when. Format: '+N["+modifierUnits+"][@hh:mm]'")
 
-	// TODO: support repeats
+	// TODO: support repeats?
+	// endFlag := flag.String("end", "", "(optional) The end, relative to 'start'. Format: '+N["+modifierUnits+"][@hh:mm]'")
 	// interval := flag.String("i", "1", "how many repeats?")
 	// unit := flag.String("u", "days", "unit for repeating the event")
 
@@ -125,42 +171,41 @@ func main() {
 	// Print help
 	if *help {
 		flag.PrintDefaults()
-		return
+		os.Exit(0)
 	}
 
-	if *debug {
-		fmt.Printf("%v,%v\n", *startFlag, *endFlag)
+	// parse start time
+	start, err := parseTimeString(*startFlag, carbon.Now())
+	if err != nil {
+		fmt.Printf("Something went wrong: %s\n", err.Error())
+		os.Exit(1)
 	}
 
-	// parse time
-	now := parseTimeString(*startFlag)
-	fmt.Printf("%v", now)
+	// parse end time
+	// end, err := parseTimeString(*endFlag, start)
+	// if err != nil {
+	// 	fmt.Println("Something went wrong!")
+	// 	return
+	// }
 
 	// create from CLI args (with sane defaults)
 	entry := Reminder{
 		Message: *msgFlag,
+		Start:   float64(start.Timestamp()),
+		// End:     float64(end.Timestamp()),
 	}
 
-	// if *start. {
-	// 	entry.Start = *start
-	// }
+	// append to file
 	var reminders []Reminder
-
-	// parse and append
-	_, err := readDb("./tst.json", &reminders)
-	if err != nil {
-		log.Panic().Caller().Float64("took", time.Since(benchMark).Seconds()).Msg(err.Error())
-	}
-
+	readDb("./tst.json", &reminders)
 	reminders = append(reminders, entry)
-	success, err := writeDb("./tst.json", reminders)
-	if err != nil {
-		log.Error().Float64("took", time.Since(benchMark).Seconds()).Msg(err.Error())
-	}
-
-	if success {
-		fmt.Println("Successfully added the reminder!")
-	}
+	success, _ := writeDb("./tst.json", reminders)
 
 	log.Debug().Caller().Float64("took", time.Since(benchMark).Seconds()).Send()
+	if success {
+		fmt.Printf("Successfully added a reminder for '%s' @ %s \n", *msgFlag, start.ISO8601String())
+		os.Exit(0)
+	}
+
+	os.Exit(1)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/bruno-domazet/go-reminder
 
 go 1.15
 
-require github.com/rs/zerolog v1.20.0
+require (
+	github.com/rs/zerolog v1.20.0 // indirect
+	github.com/uniplaces/carbon v0.1.6
+)
